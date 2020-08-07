012c575345b368b5e705155cef416c6e8012c9e3
CarlKarlQarl <karlmatthes@gmail.com>
2020-08-07 15:47:38 -0600

    Added SceneLoader to each scene, and all scenes can be navigated to now. also appended a modulo division on the next scene increment, so that when it reaches the final scene, it goes back to the first.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour {
     public void LoadNextScene(){
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        SceneManager.LoadScene((currentSceneIndex + 1) % 3);
     }
 }
